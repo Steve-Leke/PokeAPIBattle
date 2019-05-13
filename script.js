33989d8c2d49596c4684f3f6946df71f75228e0e
Steve <stevel@csedge.org>
2019-05-13 08:11:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 $(document).ready(function(){
 
+//click the button YES
+//triggers function to get pokemon from API YES
+//display on page {picture, name, and relevant data you want}
+
+let leftPokemon;
+let rightPokemon;
 
 //starter function to pull a random pokemon from the PokeAPI
-  function getRandomPokemonLeft() {
+function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
-      console.log("Left Pokemon Data: ", pokeData);
+      leftPokemon = pokeData;
+
+      let leftPokemonDisplay = `
+      <h2>${leftPokemon['name']}</h2>
+      <img src="${leftPokemon['sprites']['front_default']}">
+      `
+
+      $("#left").empty();
+      $("#left").append(leftPokemonDisplay);
     });
-  }
+}
 
+$("#leftButton").click(getRandomPokemonLeft)
 
+//starter function to pull a random pokemon from the PokeAPI
+function getRandomPokemonRight() {
+    let randomNumber = Math.floor(Math.random() * 800) + 1;
+    $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+      rightPokemon = pokeData;
 
-//all code here
+      let rightPokemonDisplay = `
+      <h2>${rightPokemon['name']}</h2>
+      <img src="${rightPokemon['sprites']['front_default']}">
+      `
 
+      $("#right").empty();
+      $("#right").append(rightPokemonDisplay);
+    });
+}
+
+$("#rightButton").click(getRandomPokemonRight)
 
 
 
